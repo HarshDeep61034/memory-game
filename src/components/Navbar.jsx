5f546769e53d18386184de12c23d5cd4731b2d3b
Harsh Deep <hdeep61034@gmail.com>
2023-07-30 19:11:26 +0530

    Update Navbar.jsx
    fixed logo bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import '../styles/Navbar.css'
+import logo from '../../public/pokeball.png'
 function Navbar(props){
     return(
         <>
         <div className="nav">
             <div className="heading">
-                <img className='logo' src="../../public/pokeball.png" alt="pokeball" />
+                <img className='logo' src={logo} alt="pokeball" />
             <h1> Pika-Pika-Poo Game</h1>
                 </div> 
             <p className='score'>score: {props.score}</p>
