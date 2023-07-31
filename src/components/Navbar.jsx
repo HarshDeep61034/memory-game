0e3e98022e88d56daf6262c90ae0292e578d2183
Harsh Deep <hdeep61034@gmail.com>
2023-07-31 15:43:03 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import '../styles/Navbar.css'
-import {logo} from '../../public/pokeball.png'
 function Navbar(props){
     return(
         <>
         <div className="nav">
             <div className="heading">
-                <img className='logo' src={logo} alt="pokeball" />
+                <img className='logo' src="../../public/pokeball.png" alt="pokeball" />
             <h1> Pika-Pika-Poo Game</h1>
                 </div> 
             <p className='score'>score: {props.score}</p>
